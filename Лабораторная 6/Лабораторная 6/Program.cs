83934fa5a43f37e80e8d98e3d9484af80a210515
Vasilisa Kashperko <vasilisika@gmail.com>
2023-03-31 10:25:03 +0300

    Final version

---
+++ Лабораторная 6/Лабораторная 6/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace Лабораторная_6
+{
+    class Program
+    {
+        public static readonly int n = 253; // 11 * 23
+        public static readonly int x = 5;
+        public static readonly int length = 13;
+
+        // ПСП генератор на основе квадратичных вычетов
+        // Вычисляет каждое число последовательности отдельно по формуле квадратичных вычетов
+        public static int BBSnext(int previous, int index)
+        {
+            int resultForSequence = (previous * previous) % n; // x_t= (x_(t-1))^2 mod n, вычисляемое значение генератора = (предыдущий элемент псп)^2 modn
+            Console.WriteLine($"x{index} = ({previous}*{previous})mod {n} = {resultForSequence}");
+            return resultForSequence;
+        }
+
+        static void Main(string[] args)
+        {
+            // B B S
+
+            int[] seq = new int[length]; //хранит числа генератора псп
+
+            Console.WriteLine($"n = {n} - число Блюма");
+            Console.WriteLine($"x = {x}\n");
+            int buf = x;
+
+            long OldTicks = DateTime.Now.Ticks;
+
+            for (int i = 0; i < length; i++)
+            {
+                buf = BBSnext(buf, i); //начальное значение х = 5, взаимнопростое с числом Блюма n 
+                seq[i] = buf;
+            }
+            Console.Write("\nПСП = ");
+            foreach (int item in seq)
+            {
+                Console.Write($"{item}; ");
+            }
+
+            Console.WriteLine($"\nВремя зашифрования: {(DateTime.Now.Ticks - OldTicks) / 1000} мс");
+
+            // RC4
+
+            Console.WriteLine("\n\n\n RC4 \n");
+
+            int[] ikey = { 43, 45, 100, 21, 1 };
+            byte[] key = new byte[ikey.Length];
+
+            for (int i = 0; i < ikey.Length; i++)
+            {
+                key[i] = Convert.ToByte(ikey[i]);
+            }
+
+            RC4 rc = new RC4(key);
+            RC4 rc2 = new RC4(key);
+            byte[] testBytes = Encoding.ASCII.GetBytes("Kashperko Vasilisa");
+
+            OldTicks = DateTime.Now.Ticks;
+
+            byte[] encrypted = rc.Encode(testBytes, testBytes.Length);
+            Console.WriteLine($"Зашифрованнное сообщение: {Encoding.ASCII.GetString(encrypted)}");
+
+            Console.WriteLine($"Время зашифрования: {(DateTime.Now.Ticks - OldTicks) / 1000} мс");
+
+            OldTicks = DateTime.Now.Ticks;
+
+            byte[] decrypted = rc2.Encode(encrypted, encrypted.Length);
+            Console.WriteLine($"Рашифрованнное сообщение: {Encoding.ASCII.GetString(decrypted)}");
+
+            Console.WriteLine($"Время расшифрования: {(DateTime.Now.Ticks - OldTicks) / 1000} мс");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лабораторная 6/Лабораторная 6/RC4.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+
+namespace Лабораторная_6
+{
+    public class RC4
+    {
+        byte[] S = new byte[256];
+        int x = 0;
+        int y = 0;
+
+        public RC4(byte[] key)
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                S[i] = (byte)i;
+            }
+
+            int j = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                j = (j + S[i] + key[i % key.Length]) % 256; // j = (j + Si + Ki) mod 256;
+                S.Swap(i, j); // меняем местами        
+            }
+        }
+
+        // в параметрах передается массив исходных байтов и их размер
+        // для каждого байта массива исходных данных запрашивает байт ключа
+        // и объединяет их при помощи xor (^)
+        public byte[] Encode(byte[] dataB, int size)
+        {
+            byte[] data = dataB.Take(size).ToArray();
+            byte[] cipher = new byte[data.Length];
+
+            for (int m = 0; m < data.Length; m++)
+            {
+                cipher[m] = (byte)(data[m] ^ ItemKey());
+            }
+            return cipher;
+        }
+
+
+        // При каждом вызове отдает следующий байт ключевого потока,
+        // который мы будем объединять xor'ом с байтом исходных данных
+        // Генератор ПСП
+        private byte ItemKey()
+        {
+            x = (x + 1) % 256;
+            y = (y + S[x]) % 256;
+
+            S.Swap(x, y);
+
+            return S[(S[x] + S[y]) % 256];
+        }
+    }
+
+    static class SwapExt
+    {
+        public static void Swap<T>(this T[] array, int index1, int index2)
+        {
+            T temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+    }
+}
